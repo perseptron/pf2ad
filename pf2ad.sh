f7a4cb5ccdd321f6b72819486c3c8d9c090b05df
JOSÉ CARDOSO DOS SANTOS NETO <ddeviljo@gmail.com>
2020-10-14 12:00:44 -0300

    Update pf2ad.sh

--- pf2ad.sh
+++ pf2ad.sh
@@ -3,7 +3,7 @@
 VERSION='20180302001' # Welcome to Brasil
 
 if [ -f "/etc/samba.patch.version" ]; then
-	if [ "$(cat /etc/samba.patch.version)" != "$VERSION" ]; then
+	if [ "$(cat /etc/samba.patch.version)" = "$VERSION" ]; then
 		echo "ERROR: Changes have been applied!"
 		echo "$VERSION"
 		exit 2
